77bc36ec08776abac0d032ee89526840c9b77adb
jo <jo@Appleui-MacBook-Pro.local>
2019-07-10 17:52:46 +0900

    added 'unfolding/folding pages' with javascript action

--- scrape_periodicals.py
+++ scrape_periodicals.py
@@ -1,99 +1,109 @@
 #coding: utf-8
 #2018.10, jek.cl.nlp@gmail.com
 
+import pdb
 import requests
 import dataset
 from datetime import datetime
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import time, platform
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+system_name = platform.system()
+
+if system_name == 'Linux':
+    browser = webdriver.Firefox()
+else: # Windows, Darwin(Mac)
+    browser = webdriver.Chrome()
 
 def get_periodical_list(meta_url, tablename):
-    r = requests.get(meta_url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    p_lis = soup.find('tbody').find_all('tr')
-    for p in p_lis:
-        atts = p.find_all('td')
-        title = atts[0].get_text(strip=True)
-        p_date = atts[1].get_text(strip=True)
-        url_xx = p.find(class_='c_pitb ellip').find('a').get('href')
-        url_id = url_xx.split(',')[1].strip().strip("'")
+    browser.get(meta_url)
+    span = browser.find_element_by_class_name('btnWline')
+    if span:
+        span.click()
+        time.sleep(2)
+
+    b_list=browser.find_elements_by_class_name('tbl_bbs_list')
+    assert len(b_list) == 1
+    p_list=b_list[0].find_elements_by_tag_name('tbody')[0].find_elements_by_tag_name('tr')
+    for p in p_list:
+        a = p.find_element_by_tag_name('a')
+        a_href = a.get_attribute('href')
+        tds= p.find_elements_by_tag_name('td')
+        assert len(tds) > 1
+        p_date=tds[1].text
+        title = a.text
+        url_id = a_href.split(',')[1].replace("'", '') #javascript:goItemView('ma', 'ma_087', '')
+        print(title, '\t', url_id, p_date)
         a_record = {'top_id':url_id, 'title':title, 'p_date':p_date}
         db[tablename].upsert(a_record, ['top_id'])
-    return len(p_lis)
+
+    '''if span :
+        span.click()
+        time.sleep(2)'''
+    #browser.quit()
+    return len(p_list) # it should be 83 in 2019.6
+
 
 def scrape_vids(req_id, tablename):
     #db.query("drop table %s" %(tablename))
-    req_url = base_url + req_id
-    r = requests.get(req_url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    p_id=soup.find('ul', class_='data_list_step step01').get('parentid')
+    browser.get(base_url+req_id)
 
-    lis = soup.find_all('div', attrs={'class':'dl_liCont'})
-    v_ids = [item.get('id') for item in lis]
+    # if only there is more(전체보기)
+    folder = browser.find_element_by_class_name('btnWline')
+    if folder :
+        folder.click() # unfold
+        time.sleep(2)
+
+    lis = browser.find_elements_by_class_name('dl_liCont')
     for item in lis:
-        v_id = item.get('id')
-        title = item.find(class_='expandable').get_text(strip=True)
-        p_date = item.find(class_='dlP01').get_text(strip=True)
+        v_id = item.get_attribute('id')
+        title = item.find_element_by_class_name('expandable').text
+        p_date = item.find_element_by_class_name('dlP01').text
         v_url = urljoin(base_url, v_id)
-        a_record= {'url' : v_url, 'pid': p_id, 'vid': v_id, 'title': title, 'p_date': p_date, 'last_seen' : datetime.now() }
+        a_record= {'url' : v_url, 'pid': req_id, 'mid': v_id, 'title': title, 'p_date': p_date, 'last_seen' : datetime.now() }
         db[tablename].upsert(a_record, ['url'])
-    return v_ids
 
-def scrape_mids_in_js(top_id):
-    from selenium import webdriver
-    from selenium.webdriver.common.keys import Keys
-    import time, platform
-    system_name = platform.system()
-    outfile_name = top_id + '.url.txt'
-    outfile = open(outfile_name, 'w', encoding='utf-8')
-    if system_name == 'Linux':
-        browser = webdriver.Firefox()
-    else: # Windows, Darwin(Mac)
-        browser = webdriver.Chrome()
+    return len(lis)
+
+def scrape_mids(top_id, tablename):
     top_url = base_url + top_id
     browser.get(top_url)
+    folder = browser.find_element_by_class_name('btnWline')
+    if folder:
+        folder.click()
+        time.sleep(3)
+
+    ids_title = []
     lis = browser.find_elements_by_class_name('dl_liCont')
     for i, li in enumerate(lis, 1):
-        li_id = li.get_attribute('id')
+        p_id = li.get_attribute('id')
+        p_date = li.find_element_by_class_name('dlP01').text
         li_strong = li.find_element_by_tag_name('strong')
-        print(li_id, li_strong.text) #fold
-        li_strong.click()
-        time.sleep(2)
+        print(p_id, li_strong.text)
+        li_strong.click() #unfold
+        time.sleep(3)
         #XXX: javascript function onclick="retrieveListItemLevelChild('ma_013_0010',0)
-        sub_li = browser.find_elements_by_xpath("//ul[@parentid='%s']/li"%li_id)
+        sub_li = browser.find_elements_by_xpath("//ul[@parentid='%s']/li"%p_id)
         for sli in sub_li:
             a = sli.find_element_by_tag_name('a')
             a_href = a.get_attribute('href')
-            print(li_id,'\t', a.text, '\t', a_href, file=outfile)
-
-        li_strong.click() #unfold
-        time.sleep(2)
+            title = a.text
+            s_id = a_href.split(',')[1].replace('"', '')
+            v_url = urljoin(base_url, s_id)
+            a_record= {'url' : v_url, 'pid': p_id, 'mid': s_id, 'title': title, 'p_date': p_date, 'last_seen' : datetime.now() }
+            db[tablename].upsert(a_record, ['url'])
+            ids_title.append((p_id, s_id, title))
+        li_strong.click() #fold
+        time.sleep(3)
     browser.quit()
-    outfile.close()
-    return outfile_name
-
-def make_vid_mid_title(infn):
-    import re, sys
-    ids_title = []
-    infile = open(infn, 'r', encoding='utf-8')
-    for l in infile:
-        tab = l.strip().split('\t')
-        if len(tab) != 3 :
-            print('You should never see this:', l, file=sys.stderr)
-            exit()
-        part=tab[2][tab[2].find('javascript'):]
-        mid = part.split('","')
-        #print(tab[0], mid[1], tab[1])
-        ids_title.append((tab[0], mid[1], tab[1])) # (vid, mid, title)
-    infile.close()
     return ids_title
 
-def put_vid_mid(base_url, vid_mid_list, ids_table, article_table):
+def scrape_articles(base_url, vid_mid_list, article_table):
     for (vid, mid, title) in vid_mid_list:
         print(vid, mid, title)
-        mi_url = urljoin(base_url, mid)
-        a_record = {'url': mi_url, 'vid': vid, 'mid': mid, 'title': title, 'last_seen':datetime.now()}
-        db[ids_table].upsert(a_record, ['url'])
+        m_url = urljoin(base_url, mid)
         scrape_article(base_url, mid, article_table)
     return
 
@@ -111,7 +121,14 @@ def scrape_article(base_url, article_id, article_table):
         article['title'] = meta[0].find('td').get_text(strip=True)
         article['p_date'] = meta[1].find('td').get_text(strip=True)
         article['subtitle'] = meta[2].find('td').get_text(strip=True)
-        article['a_type'] = meta[3].find('td').get_text(strip=True)
+        #if there's no author, meta[] ends with [3], otherwise, with[4]
+        meta_name = meta[3].find('th').get_text(strip=True)
+        if meta_name == '필자':
+            article['author'] = meta[3].find('td').get_text(strip=True)
+            article['a_type'] = meta[4].find('td').get_text(strip=True)
+        else:
+            article['author'] = ''
+            article['a_type'] = meta[3].find('td').get_text(strip=True)
 
         article['body'] = body_soup[0].get_text(strip=True)
         db[article_table].upsert(article, ['hoi'])
@@ -122,10 +139,10 @@ def scrape_article(base_url, article_id, article_table):
 def do_a_periodical(top_id):
     ids_table = 'urls_title_%s' %top_id
     article_table = 'article_body_%s' %top_id
-    scrape_vids(top_id, ids_table)
-    id_fn=scrape_mids_in_js(top_id)
-    ids_title = make_vid_mid_title(id_fn)
-    put_vid_mid(base_url, ids_title, ids_table, article_table)
+    vnum=scrape_vids(top_id, ids_table)
+    print('volum #:', vnum)
+    ids_title=scrape_mids(top_id, ids_table)
+    scrape_articles(base_url, ids_title, article_table)
 
 
 if __name__ == "__main__":
@@ -133,19 +150,28 @@ if __name__ == "__main__":
     import os
     ddir ='data'
     if not os.path.exists(ddir): os.mkdir(ddir)
-    db = dataset.connect('sqlite:///%s/mperiodicals.db' %ddir)
+    db = dataset.connect('sqlite:///%s/2mperiodicals.db' %ddir)
     meta_url ='http://db.history.go.kr/item/level.do?itemId=ma'
 
     try:
         get_periodical_list(meta_url, 'top_meta')
     except Exception as e:
         print(e)
+        exit()
 
-    base_url = 'http://db.history.go.kr/id/'
-    for i in range(1,96):
-        top_id = format(i, '03d')
-        try:
-            do_a_periodical('ma_%s' %top_id)
-        except Exception as e:
-            print(e)
-        print('ma_%s' %top_id)
+    try:
+        #below are the same with base_url + top_id
+        # http://db.history.go.kr/item/level.do?sort=levelId&dir=ASC&start=1&limit=20&page=1&pre_page=1&setId=-1&prevPage=0&prevLimit=&itemId=ma&types=&synonym=off&chinessChar=on&brokerPagingInfo=&levelId=ma_001&position=-1
+       # http://db.history.go.kr/item/level.do?sort=levelId&dir=ASC&start=1&limit=20&page=1&pre_page=1&setId=-1&prevPage=0&prevLimit=&itemId=ma&types=&synonym=off&chinessChar=on&brokerPagingInfo=&levelId=ma_013&position=-1
+        base_url = 'http://db.history.go.kr/id/'
+        #for i in range(1,2): #96):
+        for i in range(13,14):
+            top_id = format(i, '03d')
+            try:
+                do_a_periodical('ma_%s' %top_id)
+            except Exception as e:
+                print(e)
+            print('ma_%s' %top_id)
+    except Exception as e:
+        print(e)
+
