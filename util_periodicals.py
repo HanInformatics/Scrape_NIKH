8a25ac17b019ee93ad667699b1b99113bca15af6
jo <jo@Chrisui-MacBook-Pro.local>
2020-08-07 15:25:09 +0900

    for making armistice talk text

--- util_periodicals.py
+++ util_periodicals.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 #2018.10
 import dataset
+import re
 
 def txt2table(fn, cols, dbname, tablename):
     db = dataset.connect('sqlite:///%s' %dbname)
@@ -23,21 +24,30 @@ def txt2table(fn, cols, dbname, tablename):
         db[tablename].upsert(record, ['idx'])
 
 
-def table2txt(dbname, tablename, cols, fn):
+def table2txt(dbname, tablename, cols='', fn=''):
     db = dataset.connect('sqlite:///%s' %dbname)
-    outfile = open(fn, 'w', encoding='utf-8')
+    if fn != '':
+        outfile = open(fn, 'w', encoding='utf-8')
+    else:
+        outfile = open(tablename +'.txt', 'w', encoding='utf-8')
 
     records = db[tablename].all()
-    try:
+    if cols != '':
         for rec in records:
             item = []
             for col in cols :
                 if rec[col] == None: rec[col]=''
-                item.append(rec[col])
+                item.append(re.sub(r"[\n\t]+", " ", rec[col])) # for armistice talks'''
             outfile.write('%s\n' %('\t'.join(item)))
-    except Exception as e:
-        print(e)
+    else:
+        items = []
+        for rec in records :
+            item = [str(val).strip() for col, val in rec.items()]
+            items.append(item) 
+        for item in items:
+            outfile.write('%s\n' %('\t'.join(item)))
     outfile.close()
+
 
 def xlsx2table(dbname, tablename, fn):
     import pandas as pd
@@ -64,7 +74,11 @@ def table2xlsx(dbname, tablename, fn):
 if __name__ == "__main__":
     try:
         #table2txt('data/Gaebyeok.sqlite3', 'article_body_ma_013', ['p_date', 'author', 'body'], 'ma_013.txt')
-        table2txt('data/Gaebyeok.sqlite3', 'article_body_ma_031', ['p_date', 'author', 'body'], 'ma_031.txt')
+        #table2txt('data/Gaebyeok.sqlite3', 'article_body_ma_031', ['p_date', 'author', 'body'], 'ma_031.txt')
+        #table2txt('../armistice.sqlite3', 'article_body_pn_001')
+        for i in range(1,11): 
+            table2txt('../armistice.sqlite3', 'article_body_pn_'+str(i).zfill(3), \
+            ['hoi', 'title','p_date', 'appendix', 'csdan'], 'article_body_pn_' + str(i).zfill(3) + '.txt')
         '''
         xlsx2table('data/kdb.db', 'students', 'students.xlsx')
         '''
